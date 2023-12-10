40b9fa88f5bf706852a51d26da93f177696cad50
lv7399@SterlingCS ~ % <lv7399@SterlingCS>
2023-12-09 20:01:53 -0600

    Added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { useState } from 'react'
+import { puppyList } from './data.js'
+import './App.css'
 
+function App() {
+  const [puppies, setPuppy] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
 
+console.log("puppyList: ", puppyList);
 
-      <div>
-      </div>
+function handleClick(event) {() => {
+  console.log("puppy id: ", puppy.id)
+}};
+}
+
+  return (
+    <div className="App"> 
+    {puppies.map((puppy) => {
+      return (
+        <p onClick={handleClick} key={puppy.id}>
+          {puppy.name}
+        </p>
+      );
+    })}
+  </div>
+);
+
+export default App
+//const target = event.target
